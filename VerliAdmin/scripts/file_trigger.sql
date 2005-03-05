9e9e5c4774dced67c87789347adf867336290bc2
zipi <zipi>
2005-03-05 13:02:45 +0000

    no need for it

--- VerliAdmin/scripts/file_trigger.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `file_trigger` (
-  `command` varchar(15) NOT NULL default '',
-  `send_as` varchar(15) default 'hub-security',
-  `def` text,
-  `descr` text,
-  `min_class` int(11) default NULL,
-  PRIMARY KEY  (`command`)
-) TYPE=MyISAM;
